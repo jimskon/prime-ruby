f9d83efa1b59287a9de62de37dec87ef93a31212
James Skon <skonjp@gmail.com>
2024-01-22 20:42:38 +0000

    First Commit

---
+++ prime.rb
@@ -0,0 +1,26 @@
+
+def checkPrime(n)
+    prime = true
+    i = 2
+    while prime and i<=Math.sqrt(n)
+        if n % i == 0
+            prime = false
+        end
+        i+=1
+    end
+    prime
+end
+
+def eratosthenes(n)
+    a=[]
+    for i in 1..n
+        if checkPrime(i)
+            a<<i
+        end
+    end
+    a
+end
+
+for i in 1..20
+    print eratosthenes(i),"\n"
+end
